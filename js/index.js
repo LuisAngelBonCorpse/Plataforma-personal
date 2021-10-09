4a00a054f9a150ba8d9c23fe89d0b48f1d45239e
Luis Angel Duran Lopez <luisadl591@gmail.com>
2021-10-09 17:37:28 -0500

    se sube consumo de API de Unsplash

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 const accesKey = "CMHGZ2OuImP-1-ErqNcAavXUHQz-VNpytJpQIubE3-s";
 const endPoint = 'https://api.unsplash.com/search/photos';
 
+const $btnSearchI = document.querySelector('#searchI');
+const $keyWords = document.querySelector('#keyWords');
+
+
+const idImagenes = ["img1", "img2","img3", "img4","img5"]
+
 
 async function getImages(query) {
   let response = await fetch(endPoint + '?query=' + query + '&client_id=' + accesKey);
@@ -8,17 +14,60 @@ async function getImages(query) {
   let imagesList = await jsonResponse.results;
   
   function createImages(imagesList){
-    for(let i = 0 ; i < imagesList.length ; i++ ) {
-    const image = document.createElement('img');
-    image.src = imagesList[i].urls.thumb;
-    document.body.appendChild(image);
-    }
-  }  
+    document.querySelector('#resultadosI').innerHTML = `<div id="carouselExampleIndicators"   class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active" id="img1">
+        
+      </div>
+      <div class="carousel-item" id="img2">
+        
+      </div>
+      <div class="carousel-item" id="img3">
+        
+      </div>
+      <div class="carousel-item" id="img4">
+        
+      </div>
+      <div class="carousel-item" id="img5">
+        
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+    </div>`;
+    
+    for(let i = 0 ; i < idImagenes.length; i++ ) {     
+      
+      const item = document.getElementById(idImagenes[i]);
+      const image = document.createElement('img');
+      image.classList.add('d-block');
+      image.classList.add('w-100');
+      image.src = imagesList[i].urls.thumb;
 
+      item.appendChild(image);
+
+    };
+    
+  }
   createImages(imagesList);
 }
 
-
-getImages('dog');
+$btnSearchI.addEventListener('click', () => {
+  input = $keyWords.value;
+  getImages(input.replace(' ', ','));
+});
 
 
